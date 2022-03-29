bfc0cf18b7e9f89936419e57e029359d1af7cb22
LiilB <liltrainer@pm.me>
2022-03-29 22:54:40 +0200

    Test-Driven-Development discovery

---
+++ Stack-Test/Test/Stack.test.js
@@ -0,0 +1,36 @@
+class Stack {
+	constructor() {
+		this.top = -1;
+		this.items = {};
+	}
+
+	get peek() {
+		return this.items[this.top];
+	}
+
+	push(value) {
+		this.top += 1;
+		this.items[this.top] = value;
+	}
+}
+
+describe("My Stack", () => {
+	let stack;
+
+	beforeEach(() => {
+		stack = new Stack();
+	});
+
+	it("is created empty", () => {
+		expect(stack.top).toBe(-1);
+		expect(stack.items).toEqual({});
+	});
+
+	it("can push items to the top", () => {
+		stack.push("Avocado");
+		expect(stack.top).toBe(0);
+		expect(stack.peek).toBe("Avocado");
+	});
+
+	// it('can pop off elements from the top');
+});
